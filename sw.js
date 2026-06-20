b4a4f3c4c38e816adf9581438c44e12ad6aa0666
carrotjiwon-hub <carrot.jiwon@gmail.com>
2026-06-20 14:30:13 +0900

    서비스워커를 network-first로 변경하여 캐시로 인한 구버전 노출 방지

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = "nbbang-v1";
+const CACHE_NAME = "nbbang-v2";
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(["./", "./index.html"]))
   );
@@ -10,12 +11,19 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
+// network-first: 항상 최신 버전을 우선 받아오고, 오프라인일 때만 캐시로 대체
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
